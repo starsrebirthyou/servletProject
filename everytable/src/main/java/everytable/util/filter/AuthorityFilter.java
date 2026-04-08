32072824def4dc7d89b7dd93cf5b71dab12edad7
leehw <leehw@Voyager>
2026-04-08 17:15:32 +0900

    공유 url 경로 수정

--- everytable/src/main/java/everytable/util/filter/AuthorityFilter.java
+++ everytable/src/main/java/everytable/util/filter/AuthorityFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
